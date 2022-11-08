047879ff6afc139f9601f33d73d0f1eedec937bb
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2022-11-08 14:05:18 -0500

    add defaultCommandTimeout and new test (#5)
    * add defaultCommandTimeout and new test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     chromeWebSecurity: false,
+    defaultCommandTimeout: 7000,
     e2e: {
         setupNodeEvents(on, config) {
           // implement node event listeners here
