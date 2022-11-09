1252d7b220a2f445c0fe7f85f633a801cad1cce2
Art Diuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2022-11-09 18:39:45 -0500

    make changes to the first tc (#10)

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -2,22 +2,9 @@
 
 describe('Group jScript_group', () => {
 
-    it('AT_013.001 | NavBar > Blog', () => {
+    it('AT_013.001 | NavBar > After clicking the Blog menu User is redirected to the Blog page', () => {
         cy.visit('https://openweathermap.org');
         cy.get('#desktop-menu [href*="blog"]').invoke('removeAttr', 'target').click();
-        cy.url().should('include', '/blog/category/weather');
-        cy.get('.post-list .post').should('have.length', 10);
-
-        cy.get('.post-list .post:nth-child(1) .post__title-link').invoke('removeAttr', 'target').click();
-        cy.url().should('include', '/road-new-thinking-transport-power');
-        cy.go('back');
-
-        cy.get('.post-list .post:nth-child(5) .post__title-link').invoke('removeAttr', 'target').click();
-        cy.url().should('include', '/dawn-new-climate');
-        cy.go('back');
-
-        cy.get('.post-list .post:nth-child(10) .post__title-link').invoke('removeAttr', 'target').click();
-        cy.url().should('include', '/openweather-sponsors-world-data-league');
-        cy.go('back');
+        cy.url().should('be.equal', 'https://openweather.co.uk/blog/category/weather');
     });
 });
