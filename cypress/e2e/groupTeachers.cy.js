089ff47ae1a96c2df9b47b37d2ddf50616a70e08
MariaMoroz <82988399+MariaMoroz@users.noreply.github.com>
2022-11-08 21:13:00 -0500

    AT_010.001 | Marketplace > Verify all links have the same color on the page (#8)
    * add first test AT_010.001

--- cypress/e2e/groupTeachers.cy.js
+++ cypress/e2e/groupTeachers.cy.js
@@ -1,5 +1,13 @@
+/// <reference types="cypress" />
+
 describe('group jsTeachers', () => {
-  it('visit to project', () => {
+
+  it('AT_010.001 | Marketplace > Verify all links have the same color on the page', () => {
     cy.visit('https://openweathermap.org')
+    cy.get('div#desktop-menu a[href*="marketplace"]').invoke('removeAttr', 'target').click()
+
+    cy.get('div.market-place a[href]:not(.button-round)').each(($el, index) => {
+      cy.wrap($el).should('have.css', 'color', 'rgb(235, 110, 75)')
+    })
   })
 })
