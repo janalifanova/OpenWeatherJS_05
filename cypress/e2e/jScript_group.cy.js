e7737cb81183b3125b4878263065c50c222c12c4
Sviatlana Bannikova <atevs2789@gmail.com>
2022-11-10 04:38:49 +0100

    AT_002.001 | Header > After clicking the logo user is redirected to the home page (#11)
    * heder click on logo AT_002.001
    
    * refactor

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -7,4 +7,10 @@ describe('Group jScript_group', () => {
         cy.get('#desktop-menu [href*="blog"]').invoke('removeAttr', 'target').click();
         cy.url().should('be.equal', 'https://openweather.co.uk/blog/category/weather');
     });
+
+    it('AT_002.001 | Header > After clicking the logo user is redirected to the home page', () => {
+        cy.visit('https://openweathermap.org/examples');
+        cy.get('.logo').click();
+        cy.url().should('eq', 'https://openweathermap.org/');
+     });
 });
