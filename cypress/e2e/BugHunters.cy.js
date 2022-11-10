e6daa3e8f275599fd32986cacb090ca56e16a381
Olga <112621945+Forostinko@users.noreply.github.com>
2022-11-10 19:46:23 +0300

    AT_006.001 | Main page > Sign in (#14)
    * add AT_006.001
    
    * first commit
    
    * first commit

---
+++ cypress/e2e/BugHunters.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/> 
+
+describe('groupBugHunters', () => {
+
+    it('AT_006.001 | Main page > Sign in', function () {
+        cy.visit('https://openweathermap.org/')
+        cy.get('li.user-li').contains('Sign in').click({force: true})
+        cy.get('#user_email')
+          .should('have.attr', 'placeholder', 'Enter email')
+          .type('oforostinko@gmail.com') 
+        cy.get('#user_password.form-control')
+          .should('have.attr', 'placeholder', 'Password')
+          .type('12341234')
+        
+    })
+    
+})
