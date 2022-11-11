cee3ecedf9dd425b3648c70845f6e9893b94c2f5
Elvira Avshalumov <83605521+ElviraAvshalumov@users.noreply.github.com>
2022-11-11 12:37:07 -0600

    AT_008.005 | Main menu > Verify the user be redirected to new URL by clicking "Guide

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -2,6 +2,13 @@
 
 describe('group Ark', () => {
 
+
+  it(`AT_008.005 | Main menu > Verify the user be redirected to new URL by clicking "Guide"`, () => {
+    cy.visit("https://openweathermap.org/");
+    cy.get('a[href="/guide"]').contains("Guide").click();
+    cy.url().should("eq", "https://openweathermap.org/guide");
+  }); 
+  
 it('AT_010.004 | Marketplace > Verify all orange links on the page', () => {
     cy.visit('https://openweathermap.org/')
     cy.get('#desktop-menu [href*=market]').invoke('removeAttr', 'target').click()
