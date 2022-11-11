e126140ac35038c0383eae3dc2f50cfa3679178d
lordlor444 <lordlor23@gmail.com>
2022-11-10 22:50:09 -0800

    addHello

--- cypress/e2e/group_javascript_for_qas.cy.js
+++ cypress/e2e/group_javascript_for_qas.cy.js
@@ -3,5 +3,6 @@
 describe('group_javascript_for_qas', () => {
     it('AT_028.001 | Footer > About us > Verify "About us" link redirects to the corresponding page', function () {
         cy.visit('https://openweathermap.org/');
+        console.log("Hello");
     });
 });
