3b7a52a9f0fe7941be9169056b52324bd84da1d0
Klasha_G <68085349+kgoldpasv@users.noreply.github.com>
2022-11-11 10:20:12 -0800

    AT_030.002 | Footer > After clicking on the `Website terms and conditions` user navigated to a new pdf file page (#19)
    * arkgroup first commit
    
    * made file with one it
    
    Co-authored-by: Penshin <82130724+Penshin@users.noreply.github.com>

--- cypress/e2e/arkGroup.cy.js
+++ cypress/e2e/arkGroup.cy.js
@@ -12,5 +12,13 @@ it('AT_010.004 | Marketplace > Verify all orange links on the page', () => {
       })
     })
   });
-  
+
+    it('AT_030.001|Footer>Verify redirection to terms and conditions', function() {
+        cy.visit('https://openweathermap.org/')
+      
+        cy.get('div.footer-section a[href*="Openweather_website_terms_and_conditions"]')
+        .invoke("removeAttr", "target")
+        .click()                  
+         cy.url().should('include', 'website_terms_and_conditions_of_use.pdf')
+    })
 });
