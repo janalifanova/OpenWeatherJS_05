c283ac589e4052ef7b3debb48e339be918f27fee
EkaterinaRomankova <catia.romankova@yandex.by>
2022-11-12 14:05:41 +0300

    second_logo

--- cypress/e2e/ltByJs.cy.js
+++ cypress/e2e/ltByJs.cy.js
@@ -1,13 +1,13 @@
 /// <reference types="cypress"/>
 
 describe('Group lt_by_js', () => {
-    xit('AT_033.001 | Header > Navigation > Verify "Dashboard" menu item', () => {
+    it('AT_033.001 | Header > Navigation > Verify "Dashboard" menu item', () => {
         cy.visit('https://openweathermap.org');
         cy.get('#desktop-menu [href$=-dashboard]').click();
         cy.url().should('include', '/weather-dashboard');
     });
 
-    xit('AT_033.002 | Header > Navigation > Verify "Guide" menu item', () => {
+    it('AT_033.002 | Header > Navigation > Verify "Guide" menu item', () => {
         cy.visit('https://openweathermap.org/')
         cy.get('#desktop-menu [href="/guide"]').click()
         cy.url().should('eq', 'https://openweathermap.org/guide')
@@ -17,6 +17,7 @@ describe('Group lt_by_js', () => {
         cy.visit('https://openweathermap.org/')
         cy.get('#desktop-menu a[href="/our-initiatives"]').click();
         cy.get('.logo').click()
+        cy.url().should('eq', 'https://openweathermap.org/');
         
     });
     
