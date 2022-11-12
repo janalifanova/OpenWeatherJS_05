310cdda1690544de56b150e0195611e261f96702
Olga <112621945+Forostinko@users.noreply.github.com>
2022-11-12 18:36:30 +0300

    add code (#55)

--- cypress/e2e/BugHunters.cy.js
+++ cypress/e2e/BugHunters.cy.js
@@ -11,7 +11,10 @@ describe('groupBugHunters', () => {
         cy.get('#user_password.form-control')
           .should('have.attr', 'placeholder', 'Password')
           .type('12341234')
-        
+        cy.get('#user_remember_me').check().should('be.checked')
+        cy.contains('Submit').click()
+        cy.get('.panel-body').should('have.text','Signed in successfully.')
+  
     })
 
     it ('AT_029.001 | Two icons "Download on the App store" and "Get it on Google play" are visible', function() {
