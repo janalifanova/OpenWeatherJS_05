64457444495104fd0c7aa0d5af9f26682026dc64
OLgaByR <114132439+OLgaByR@users.noreply.github.com>
2022-11-12 15:29:57 -0600

    AT_009.001 (#62)

--- cypress/e2e/ltByJs.cy.js
+++ cypress/e2e/ltByJs.cy.js
@@ -31,5 +31,13 @@ describe('Group lt_by_js', () => {
         cy.url().should('eq', 'https://openweathermap.org/')
 
     });
+
+    it('AT_009.001 | Main menu > After clicking Marketplace User is redirected to the Marketplace page', () => {
+        const marketplace = '#desktop-menu a[href*="marketplace"]'
+
+        cy.visit('https://openweathermap.org/')
+        cy.get(marketplace).invoke('removeAttr', 'target').click()
+        cy.url().should('eq', 'https://home.openweathermap.org/marketplace')
+    });
     
 });
