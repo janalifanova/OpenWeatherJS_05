2f8b7afddc0722d2baf1f3c7d5506a28a0fe1200
Illia Shabaiev <ilyashabaev2209@gmail.com>
2022-11-12 23:37:29 -0600

    AT_050.002 | Footer > Verify that user can be redirected to the 'Term… (#70)
    * AT_050.002 | Footer > Verify that user can be redirected to the 'Terms and conditions of sale' page
    
    * made changes based on Tims comment

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -118,6 +118,11 @@ describe('Group jScript_group', () => {
         cy.get('div#desktop-menu a[href*="examples"]').click();
         cy.get('a[href="http://wordpress.org/extend/plugins/awesome-weather/"]').invoke('removeAttr', 'target').click();
         cy.url().should('eq', 'https://wordpress.org/plugins/awesome-weather/');
-    })  
-});
+    });
 
+    it('AT_050.002 | Footer > Verify that user can be redirected to the "Terms and conditions of sale" page', () => {
+        cy.visit('https://openweathermap.org');
+        cy.get('[href*="conditions_of_sale"]').invoke('removeAttr', 'target').click()
+        cy.url().should('eq', 'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf')
+    });
+});
