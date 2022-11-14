6c248405ef0f290cecae0fb67ea1ba7878cf1499
SvetlanaMag <svetlanammmsv@gmail.com>
2022-11-14 17:23:52 +0300

    add first test AT_088.007

--- cypress/e2e/BugHunters.cy.js
+++ cypress/e2e/BugHunters.cy.js
@@ -44,4 +44,10 @@ describe('groupBugHunters', () => {
       cy.url().should('eq', 'https://play.google.com/store/apps/details?id=uk.co.openweather')        
     })
 
+    it('AT_008.007 | Main menu > Guide > Verify user will be redirected to new url "/guide"', () => {
+      cy.visit('https://openweathermap.org/')
+      cy.get('#desktop-menu a[href="/guide"]').click()
+      cy.url().should('include', '/guide')
+  });
+
 })
