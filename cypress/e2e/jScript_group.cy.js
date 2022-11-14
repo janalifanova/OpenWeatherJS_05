679ea0ce32173b1a1b8c9691a5af52aa63a14929
Art Diuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2022-11-14 18:44:45 -0500

    AT_013.003 | Blog > Weather > Verifying the first post's link is clickable and redirects User to the post in a different tab (#131)
    * add new test
    
    * add a semicolon

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -178,7 +178,7 @@ describe('Group jScript_group', () => {
         cy.get('.logo').click();
         cy.url().should('include', 'https://openweathermap.org/');
     });
-    
+
     it('AT_005.002 | Main page > Verify the website\'s description', () => {
         cy.visit('https://openweathermap.org');
         cy.get('span.white-text').should('have.text', 'Weather forecasts, nowcasts and history in a fast and elegant way');
@@ -195,5 +195,12 @@ describe('Group jScript_group', () => {
         cy.get('h1.post-page__title')
           .should('have.text', 'The Road to a New Thinking in Transport Power')
     });     
+    
+    it('AT_013.003 | Verifying the first post\'s link is clickable and redirects User to the post on a new page', () => {
+        cy.visit('https://openweathermap.org');
+        cy.get('#desktop-menu [href*="blog"]').invoke('removeAttr', 'target').click();
+        cy.get('.post-list .post:nth-child(1) .post__title-link').click();
+        cy.url().should('include', 'https://openweather.co.uk/blog/post/');
+    });
 });
 
