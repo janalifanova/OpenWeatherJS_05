9a841efc5147a600b8475d41f6c6f5bd0a38db87
NataliiaHaiduk <83046340+NataliiaHaiduk@users.noreply.github.com>
2022-11-13 22:50:05 -0500

    fill Different weather (#98)

--- cypress/e2e/Reporter.cy.js
+++ cypress/e2e/Reporter.cy.js
@@ -4,7 +4,18 @@
 describe('GroupReporters', () => {
 
     const inputSearchCity = 'input[placeholder = "Search city"]';
-
+    const differentWeatherBtn = '.controls span.owm-switch';
+    const differentWeatherPopup = 'div.pop-up-container';
+    const differentWeatherIcon = 'ul.icons span'; // should be used with method .contains('icon text')
+    const diffWeathMoreOptions = 'div.more-options';
+    const diffWeathTemperatureField = '[type="number"]';
+    const diffWeathWindStrong = '#strong';
+    const diffWeathEmail = 'input[type="email"]';
+    const diffWeathDataSourseDropArr = '.dropdown-selector svg.icon-down';
+    const diffWeathDataSourseDropItem = 'div.menu-item span'; // should be used with method .contains('item text')
+    const diffWeathAddInfo = '.owm_textarea';
+    const diffWeathSendBtn = '.pop-up-footer .button-round';
+   
     beforeEach(function () {
         cy.visit('https://openweathermap.org/')
     });
@@ -46,6 +57,22 @@ describe('GroupReporters', () => {
         submit();
         cy.get(inputSearchCity).invoke('val').should('eq', zipCode);
     });
+
+    it('AT_024.002 | After clicking on "send" button, the form window automatically disappears', () => {
+       
+        cy.get(differentWeatherBtn).click()
+        cy.get(differentWeatherPopup).should('be.be.visible')
+        cy.get(differentWeatherIcon).contains('clear sky')
+        cy.get(diffWeathMoreOptions).click()
+        cy.get(diffWeathTemperatureField).clear({force: true}).type('50')
+        cy.get(diffWeathWindStrong).click({force: true})
+        cy.get(diffWeathEmail).type('test@mail.com')
+        cy.get(diffWeathDataSourseDropArr).click()
+        cy.get(diffWeathDataSourseDropItem).contains('Personal feelings').click()
+        cy.get(diffWeathAddInfo).type('Not nice to lie about weather!')
+        cy.get(diffWeathSendBtn).click()
+        cy.get(differentWeatherPopup).should('not.exist')
+    })
 
     it('AT_034.001 | <Header > verify "For Business" button', () => {
         cy.get('#desktop-menu :nth-child(10) > a').invoke('removeAttr', 'target').click()
