c345223a8c456b422d5f502c3b07f01b70bf0858
TimothyHuxx <90024242+TimothyHuxx@users.noreply.github.com>
2022-11-14 14:29:06 -0800

    AT_005.002 | Main page > Verify the website’s description (#126)
    * verify website's description -  test

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -178,5 +178,10 @@ describe('Group jScript_group', () => {
         cy.get('.logo').click();
         cy.url().should('include', 'https://openweathermap.org/');
     });
+    
+    it('AT_005.002 | Main page > Verify the website\'s description', () => {
+        cy.visit('https://openweathermap.org');
+        cy.get('span.white-text').should('have.text', 'Weather forecasts, nowcasts and history in a fast and elegant way');
+    });
 });
 
