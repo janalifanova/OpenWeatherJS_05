41ce96a23a08c6126a92fa0a5fd2e7e63d9af09a
OLgaByR <114132439+OLgaByR@users.noreply.github.com>
2022-11-15 11:40:43 -0600

    edited AT_009.003 (#165)

--- cypress/e2e/ltByJs.cy.js
+++ cypress/e2e/ltByJs.cy.js
@@ -164,19 +164,18 @@ describe('Group lt_by_js', () => {
         
     });
 
-    it('AT_009.003 | Main menu > Marketplace verification of displayed content History bulk buttons "Documentation" and "Place order"', () => {
+    it('AT_009.003 | Main menu > Marketplace verification of displayed "Documentation" button for History bulk', () => {
+
         const marketplace = '#desktop-menu a[href*="marketplace"]'
         const documentationButton = 'div.button-container > a[href="https://openweathermap.org/history-bulk"]'
-        const placeOrderbutton = 'div.button-container a[href="/history_bulks/new"].button-round'
 
         cy.visit('https://openweathermap.org/')
         cy.get(marketplace).invoke('removeAttr', 'target').click()
         cy.get(documentationButton).should('be.visible').invoke('removeAttr', 'target').click()
-        cy.url().should('eq', 'https://openweathermap.org/history-bulk')
-        cy.go('back')
-        cy.get(placeOrderbutton).should('be.visible').click()
-        cy.url().should('eq', 'https://home.openweathermap.org/history_bulks/new' )
 
+        cy.url().should('eq', 'https://openweathermap.org/history-bulk')
+        cy.get('h1[class="breadcrumb-title"]').should('have.text', 'History Bulk')
+    
     });
 
     it('AT_022.004 | Footer > Check Medium icon is clickable', function () {
