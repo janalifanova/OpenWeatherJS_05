60842e2e1ba188ab964642d47353295e9f2692c1
Yulia Lopatko <99161649+engineeryulia@users.noreply.github.com>
2022-11-17 11:12:50 -0700

    RF (#238)

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -63,8 +63,10 @@ describe('Group jScript_group', () => {
         cy.get('#user_email').type('3065606@gmail.com');
         cy.get('#user_password.form-control').type('Qwerty1234');
         cy.get('[value="Submit"]').click({force: true});
+
         cy.get('#desktop-menu #user-dropdown .inner-user-container').click({force: true});
         cy.get('.dropdown-menu [href*="/sign_out"]').click({force: true});
+        
         cy.get('.panel-body').should('have.text', 'You need to sign in or sign up before continuing.');
     });
 
