f929204ddc52029c77446b514b5ac9284ebb7557
SviatlanaMag <103537254+SvetlanaMag@users.noreply.github.com>
2022-11-17 19:50:08 +0300

    add first test AT_041.001 (#237)

--- cypress/e2e/BugHunters.cy.js
+++ cypress/e2e/BugHunters.cy.js
@@ -165,4 +165,18 @@ it('AT_009.008 | <Menu> Marketplace > Verification than "Place order" button is 
   cy.get(placeOrderButton).should('be.visible').click() 
   cy.url().should('eq', 'https://home.openweathermap.org/history_bulks/new')
   })
+
+  it('AT_041.001 | Verify the registered user is redirected to the My API keys page', () => {
+    
+    cy.get('li.user-li a[href*="sign_in"]').click()
+    cy.get('.new_user .email').type('redroverschool@yahoo.com')
+    cy.get('#user_password').type('123456789')
+    cy.get('input[value="Submit"]').click()
+
+    cy.get('#user-dropdown').click()
+    cy.get('#user-dropdown-menu a[href="/api_keys"]').click()
+    cy.url().should('include', '/api_keys')
+    cy.get('div.alert-info').should('contain','You can generate as many API keys as needed for your subscription. We accumulate the total load from all of them.')
+      .and('be.visible')
+  })
 })
