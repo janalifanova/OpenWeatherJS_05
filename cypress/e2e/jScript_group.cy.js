ac2fe877816d08c55295b2d83434f63dbd6ce6fd
SvetlanaG1011 <103342736+SvetlanaG1011@users.noreply.github.com>
2022-11-19 04:15:19 -0700

    AT_028.0002 | Footer > About us > Verify "Contact us" button redirects user to "Questions" page (#288)
    * questionsPageHeader and questionsPageEndPoint added to fixture
    
    * AT_028.002
    
    * Update jScript_group.json
    
    * fix test

--- cypress/e2e/jScript_group.cy.js
+++ cypress/e2e/jScript_group.cy.js
@@ -419,6 +419,16 @@ describe('Group jScript_group', () => {
         cy.get('.day-list li:first-child > span').should('have.text', todaysDate);
     });
 
+    it('AT_028.002 | <Footer> About us, Verify "Contact us" button redirects user to "Questions" page', function () {
+        cy.get('a[href="/about-us"]').click();
+        cy.get('.about-us :nth-child(9) [href="https://home.openweathermap.org/questions"]')
+          .invoke('removeAttr','target')
+          .click();
+
+        cy.url().should('include', this.data.questionsPageEndPoint);
+        cy.get('.headline').should('have.text', this.data.questionsPageHeader);    
+ });
+        
     it('AT_041.002 | Header > User > My API keys > Verify that user can navigate to api keys page and see alert info message', function () {
         cy.get('.user-li a').click();
         cy.get('[class*="string email optional "]').type(this.data.loginUserEmail);
