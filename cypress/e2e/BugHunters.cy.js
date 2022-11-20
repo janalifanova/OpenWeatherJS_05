c2c83d18426931c1fce4e16f2d107a856a061a3b
Liudmila Portnova <103488693+08AlimDuiL@users.noreply.github.com>
2022-11-20 19:33:07 +0300

    Added new test (#300)

--- cypress/e2e/BugHunters.cy.js
+++ cypress/e2e/BugHunters.cy.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress"/> 
 
 
-
 describe('groupBugHunters', () => {
 
   beforeEach(function () {
@@ -80,11 +79,11 @@ describe('groupBugHunters', () => {
       .should('be.visible')
       .should('contain.text', cityName)
   })
-  
+
   it('AT_009.006 | Main menu > Marketplace > verify that user will be redirected to new URL "Marketplace', function () {
     const marketplace = '#desktop-menu a[href*="marketplace"]'
-    cy.get(marketplace).invoke('removeAttr', 'target').click({force: true})
-    cy.url().should('eq','https://home.openweathermap.org/marketplace')
+    cy.get(marketplace).invoke('removeAttr', 'target').click({ force: true })
+    cy.url().should('eq', 'https://home.openweathermap.org/marketplace')
   })
 
   it('AT_008.008 | Main menu > Guide > Verify the user redirected to new url', () => {
@@ -118,11 +117,11 @@ describe('groupBugHunters', () => {
   it('009.007 | Main menu > Marketplace > Verification of displayed "Documentation" button for History bulk', function () {
     const marketplace = '#desktop-menu a[href*="marketplace"]'
     const documentationButton = '.product a[href="https://openweathermap.org/history-bulk"]'
-    
+
     cy.get(marketplace).invoke('removeAttr', 'target').click({ force: true })
     cy.get(documentationButton).should('be.visible').invoke('removeAttr', 'target').click()
   })
-  
+
   it('AT_008.009 | Main menu > Guide > Verify text on the page', () => {
     cy.get('#desktop-menu').contains('Guide').click({ force: true });
     cy.get('.col-sm-12 h1').should('have.text', 'Weather data in a fast and easy-to-use way');
@@ -133,7 +132,7 @@ describe('groupBugHunters', () => {
     cy.get('ol > :nth-child(24)').should('have.text', 'How to start using Weather API');
   })
 
-  it('AT_056.001 | My API keys > Managing API keys> Verify creation and deletion of an API key', function() {
+  it('AT_056.001 | My API keys > Managing API keys> Verify creation and deletion of an API key', function () {
     cy.get('.user-li').as('SignInButton').click()
     cy.get('.new_user .email').as('EnterEmailField').type('redrover@mailto.plus')
     cy.get('#user_password').as('PasswordField').type('123456789')
@@ -151,15 +150,15 @@ describe('groupBugHunters', () => {
     cy.get('.col-md-6').as('NoticeCreateKey')
       .should('include.text', 'API key was created successfully').and('include.text', 'Notice').and('be.visible')
     cy.reload()
-    cy.get('@CreatedKey').should('be.visible')    
+    cy.get('@CreatedKey').should('be.visible')
     cy.get('.api-keys tr:nth-child(2) .fa-remove').as('DeleteButton').click()
     cy.get('.col-sm-offset-2').as('NoticeDeleteKey')
       .should('include.text', 'API key was deleted successfully').and('include.text', 'Notice').and('be.visible')
-    cy.get('@CreatedKey').should('not.exist')    
+    cy.get('@CreatedKey').should('not.exist')
   })
 
-  it('AT_033.016 | Header > Navigation', function() {
-    cy.get('#desktop-menu a[href="/guide"]').click({force: true})
+  it('AT_033.016 | Header > Navigation', function () {
+    cy.get('#desktop-menu a[href="/guide"]').click({ force: true })
     cy.url().should('include', '/guide')
   })
 
@@ -170,7 +169,7 @@ describe('groupBugHunters', () => {
 
     cy.get(marketplace).invoke('removeAttr', 'target').click({ force: true })
     cy.get(historyBulk).should('be.visible')
-    cy.get(placeOrderButton).should('be.visible').click() 
+    cy.get(placeOrderButton).should('be.visible').click()
     cy.url().should('eq', 'https://home.openweathermap.org/history_bulks/new')
   })
 
@@ -183,7 +182,7 @@ describe('groupBugHunters', () => {
     cy.get('#user-dropdown').click()
     cy.get('#user-dropdown-menu a[href="/api_keys"]').click()
     cy.url().should('include', '/api_keys')
-    cy.get('div.alert-info').should('contain','You can generate as many API keys as needed for your subscription. We accumulate the total load from all of them.')
+    cy.get('div.alert-info').should('contain', 'You can generate as many API keys as needed for your subscription. We accumulate the total load from all of them.')
       .and('be.visible')
   })
 
@@ -196,21 +195,21 @@ describe('groupBugHunters', () => {
     cy.get('#user-dropdown').click()
     cy.get('a[href*="/home"]').contains('My profile').click()
     cy.url().should('include', '/home')
-    cy.get('ul.nav-tabs li:nth-child(8)').should('have.class','active')
+    cy.get('ul.nav-tabs li:nth-child(8)').should('have.class', 'active')
 
     cy.get('#password_form_password').type('123456789')
     cy.get('#password_form_password_confirmation').type('123456789')
     cy.get('input[value="Change Password"]').click()
-    cy.get('.panel-body').should('be.visible').and('have.text','Password was changed successfully')
-})
+    cy.get('.panel-body').should('be.visible').and('have.text', 'Password was changed successfully')
+  })
 
-  it('AT_027.005 | Maps > Section with the scale > The scale names matches the label after selecting "Wind speed"', function() {
-    cy.get('#desktop-menu a[href="/weathermap"]'). click()
-    cy.get('label[for="Wind speed"]'). click()
+  it('AT_027.005 | Maps > Section with the scale > The scale names matches the label after selecting "Wind speed"', function () {
+    cy.get('#desktop-menu a[href="/weathermap"]').click()
+    cy.get('label[for="Wind speed"]').click()
     cy.get('.scale-details:first-child').should('contain.contain.text', 'Wind speed')
-})
+  })
 
-  it('AT_056.002 | My API keys > Managing API keys> Verify rename an API key', function() {
+  it('AT_056.002 | My API keys > Managing API keys> Verify rename an API key', function () {
     cy.get('.user-li').as('SignInButton').click()
     cy.get('.new_user .email').as('EnterEmailField').type('yurik@mailto.plus')
     cy.get('#user_password').as('PasswordField').type('12345678')
@@ -234,15 +233,29 @@ describe('groupBugHunters', () => {
 
     //Return previous name of the key
     cy.get('@EditButton').click()
-    cy.get('@EditNameField').clear().type('Default')     
-    cy.get('@SaveButton').click()    
+    cy.get('@EditNameField').clear().type('Default')
+    cy.get('@SaveButton').click()
   })
 
   it('AT_001.016 | Main page > Section with search > Search City', () => {
     const city = 'Boston';
-    cy.get('div.search').should('be.visible').click({force: true}).type(city);
+    cy.get('div.search').should('be.visible').click({ force: true }).type(city);
     cy.get('.button-round').click();
     cy.get(':nth-child(1) > [style="width: 140px;"]').click();
     cy.get('.current-container > :nth-child(1) > h2').should('contain', city);
-})
+  })
+
+  it('TC_046.002|Click on the link “Learn more” should take user to the new page', function () {
+    cy.get('#desktop-menu [href="/our-initiatives"]').click();
+    cy.get('.col-sm-12 :nth-child(9) h2>span')
+      .as('Education')
+      .should('have.text', 'Education');
+    cy.get('center a')
+      .as('LearnMore')
+      .should('have.text', 'Learn more');
+    cy.get('@LearnMore').click();
+    cy.url().should('include', '/student-initiative');
+    cy.get('h1.breadcrumb-title').should('have.text', 'Student initiative');
+  })
+
 })
