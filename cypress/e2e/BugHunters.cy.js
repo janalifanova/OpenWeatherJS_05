1232e294029055bbc3a411137ee5e682483a4bcc
yuro4ka250686 <selivanovyurik@gmail.com>
2022-11-22 12:16:08 +0300

    AT_021.005

--- cypress/e2e/BugHunters.cy.js
+++ cypress/e2e/BugHunters.cy.js
@@ -296,4 +296,21 @@ describe('groupBugHunters', () => {
     })
   })
 
+  it('AT_021.005 | Footer > Widgets> Verify redirect to Widgets constructor page', function() {
+    cy.get('.user-li').as('SignInButton').click()
+    cy.get('.new_user .email').as('EnterEmailField').type('push@mailto.plus')
+    cy.get('#user_password').as('PasswordField').type('123456789')
+    cy.get('.btn-color[value="Submit"]').as('SummitButton').click()
+
+    cy.get('.inner-footer-container li:nth-child(5) a')
+      .as('WidgetsButton')
+      .should('include.text', 'Widgets')
+      .click()
+
+    cy.url().should('include', '/widgets-constructor')
+    cy.get('.breadcrumb-title')
+      .as('TitleOnPage')
+      .should('have.text', 'Widgets constructor')
+  })
+
 })
