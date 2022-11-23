bb35f52e5ecd90fac3c895f9f52016194697979f
TimothyHuxx <90024242+TimothyHuxx@users.noreply.github.com>
2022-11-23 02:55:44 -0800

    RF | questionsPageSpec > AT_015.001 Not checking eCAPTCHA checkbox (#347)
    * Add AT_015.001 to POM
    * Update

--- cypress/pageObjects/Header.js
+++ cypress/pageObjects/Header.js
@@ -1,15 +1,27 @@
 class Header {
     elements = {
         getBlogMenuLink: () => cy.get('#desktop-menu [href*="blog"]'),
-        getPartnersMenuLink: () => cy.get('#desktop-menu a[href="/examples"]')
-    }
-     
-    clickBlogMenuLink() {
-        this.elements.getBlogMenuLink().invoke('removeAttr', 'target').click({force: true})
+        getPartnersMenuLink: () => cy.get('#desktop-menu a[href="/examples"]'),
+        getSupportDropDownMenu: () => cy.get('#support-dropdown'),
+        getAskAquestionMenuLink: () => cy.get('#support-dropdown+ul [href$="/questions"]')
     }
 
+    clickSupport() {
+        this.elements.getSupportDropDownMenu().click({ force: true });
+    };
+
+    clickAskAquestionMenuLink() {
+        this.elements.getAskAquestionMenuLink()
+            .invoke('removeAttr', 'target')
+            .click({ force: true });
+    };
+
+    clickBlogMenuLink() {
+        this.elements.getBlogMenuLink().invoke('removeAttr', 'target').click({ force: true });
+    };
+
     clickPartnersMenuLink() {
-        this.elements.getPartnersMenuLink().click({force: true});
-    }
+        this.elements.getPartnersMenuLink().click({ force: true });
+    };
 }
 export default Header;
