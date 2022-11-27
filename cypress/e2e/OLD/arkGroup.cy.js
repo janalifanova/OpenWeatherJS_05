8b4b9825a7a221cba118d565159a715f9c246476
StanislavBliss <117870982+StanislavBliss@users.noreply.github.com>
2022-11-28 01:32:12 +0200

    rf 045.005 (#427)

--- cypress/e2e/OLD/arkGroup.cy.js
+++ cypress/e2e/OLD/arkGroup.cy.js
@@ -139,19 +139,17 @@ describe('group Ark', () => {
     cy.url().should('include', '/guide');
   })
 
-  it.skip('AT_045.005 | Main page > Section with 8-day forecast. Check display of eight days from current date', function () {
-    cy.get('.daily-container ul.day-list li > span')
-      .then($elArr => {
-        expect($elArr).to.have.length(8)
-        const startDate = new Date().getTime()
-        const formatDate = { weekday: 'short', month: 'short', day: 'numeric', timeZone: 'UTC' };
-        let itemDate
-        cy.wrap($elArr).each(($el, $i) => {
-          itemDate = startDate + 86400000 * $i
-          itemDate = new Date(itemDate).toLocaleDateString('en', formatDate)
+  it('AT_045.005 | Main page > Section with 8-day forecast. Check display of eight days from current date', function () {
+    const startDate = new Date().getTime()
+    const formatDate = { weekday: 'short', month: 'short', day: '2-digit', timeZone: 'UTC' };
 
-          cy.wrap($el).should('include.text', itemDate)
-        })
+    cy.get('.daily-container ul.day-list li > span')
+      .should('have.length', 8)
+      .each(($el, i) => { 
+        let itemDate = startDate + 86400000 * i
+        itemDate = new Date(itemDate).toLocaleDateString('en', formatDate)
+        expect($el).to.be.visible
+        expect($el.text()).to.include(itemDate);
       })
   })
 
