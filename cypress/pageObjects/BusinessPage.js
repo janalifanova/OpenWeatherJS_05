dce6c121b1e6aed39e5a8361d0fd552829e4fa70
Yulia Lopatko <99161649+engineeryulia@users.noreply.github.com>
2022-11-29 19:48:16 -0700

    Update BusinessPage.js

--- cypress/pageObjects/BusinessPage.js
+++ cypress/pageObjects/BusinessPage.js
@@ -2,10 +2,10 @@ class BusinessPage {
     elements = {
         getH1Title: () => cy.get('.mobile-padding h1'),
         getAboutUsButton: () => cy.get('a.btn_block[href="#main_about"]')
-    }
+    };
 
     clickAboutUsButton() {
         this.elements.getAboutUsButton().click({ force: true });
     };
-}
+};
 export default BusinessPage;
